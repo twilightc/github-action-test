d9132963012f876de0826d2851043383afc1b0fe
tychang <timmyc2007@gmail.com>
2024-02-29 10:47:25 +0800

    .

--- src/App.tsx
+++ src/App.tsx
@@ -22,7 +22,7 @@ function App() {
           count is {count}
         </button>
         <p>
-          Edit <code>src/App.tsx</code> and save to test HMR sth. change
+          Edit <code>src/App.tsx</code> and save to test HMR nothing change
         </p>
       </div>
       <p className="read-the-docs">
